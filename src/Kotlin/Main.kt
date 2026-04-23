c3c47fb11f73d34ecf8199e67c8115daf9359de6
Mat30-cmyk <betancurmateo116@gmail.com>
2026-04-23 13:40:03 -0500

    Estructura inicial del taller

---
+++ src/Kotlin/Main.kt
@@ -0,0 +1,16 @@
+package Kotlin
+
+import ejercicios.*
+
+fun main() {
+    println("INICIANDO TALLER DE KOTLIN")
+
+    ejecutarBloque1()
+    ejecutarBloque2()
+    ejecutarBloque3()
+    ejecutarBloque4()
+    ejecutarBloque5()
+    ejecutarBloque6()
+
+    println("FIN DEL TALLER")
+}
---
+++ src/Kotlin/ejercicios/Bloque4Ciclos.kt
@@ -0,0 +1,5 @@
+package ejercicios
+
+fun ejecutarBloque4() {
+
+}
