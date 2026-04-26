8a94c85c566851283145da6a7c135879a397d1d9
Mat30-cmyk <betancurmateo116@gmail.com>
2026-04-26 14:23:25 -0500

    Terminados ejercicio Funciones

--- src/Kotlin/ejercicios/Bloque5Funciones.kt
+++ src/Kotlin/ejercicios/Bloque5Funciones.kt
@@ -1,5 +1,51 @@
-package ejercicios
+package ejercicios.funciones
 
-fun ejecutarBloque5() {
+fun greetUser(name: String): String =
+    "Hola $name, bienvenido 👋"
 
+fun addNumbers(x: Int, y: Int): Int = x + y
+
+fun drawRectangle(width: Int, height: Int = 10) {
+    println("Rectángulo -> ancho: $width | alto: $height")
+}
+
+fun convertUsdToCop(amount: Double): Double {
+    val exchangeRate = 4000.0
+    return amount * exchangeRate
+}
+
+fun runFunctionExamples() {
+    printTitle("BLOQUE 5: FUNCIONES")
+
+    // Saludo
+    println("21. ${greetUser("Jhon")}")
+    divider()
+
+    // Suma
+    val total = addNumbers(15, 30)
+    println("22. Resultado suma: $total")
+    divider()
+
+    // Default param
+    println("23. Rectángulo básico:")
+    drawRectangle(5)
+    divider()
+
+    // Named args
+    println("24. Rectángulo personalizado:")
+    drawRectangle(width = 20)
+    divider()
+
+    // Conversión
+    val dollars = 50.0
+    val converted = convertUsdToCop(dollars)
+    println("25. $dollars USD -> $converted COP")
+}
+
+fun printTitle(text: String) {
+    println("\n=== $text ===")
+}
+
+fun divider() {
+    println("---------------------------")
 }
